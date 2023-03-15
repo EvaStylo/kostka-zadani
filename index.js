bed1bc64b2a89fcf5443df5e1be6830014c13400
DESKTOP-MBRQ737\Eva <evastylova@gmail.com>
2023-03-15 20:20:13 +0100

    Ukol kostka

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log('funguju!');
+
+let cislo = 1
+const kostka = document.querySelector('.dice')
+const otoceniKostky = () => {
+	cislo = cislo + 1
+	if (cislo === 7) {
+		cislo = 1
+	}
+	kostka.src = `img/side${cislo}.svg`
+}
+document.addEventListener('keydown', otoceniKostky)
